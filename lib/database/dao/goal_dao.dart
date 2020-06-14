bef1e9eecc6e53a517d76102dffb7adca011134f
Breno Baronte <brenobaronte@hotmail.com>
2020-06-14 19:34:46 -0300

    fix add goal dao and adding setup to goal test

---
+++ lib/database/dao/goal_dao.dart
@@ -0,0 +1,55 @@
+import 'package:build/database/app_database.dart';
+import 'package:build/models/goal.dart';
+import 'package:sqflite/sqflite.dart';
+
+class GoalDao {
+  static const String tableSql = 'CREATE TABLE $_tableName('
+      '$_id INTEGER PRIMARY KEY, '
+      '$_title TEXT, '
+      '$_count INTEGER, '
+      '$_countUnit TEXT, '
+      '$_feeling INTEGER)';
+  static const String _tableName = 'goals';
+  static const String _id = 'id';
+  static const String _title = 'title';
+  static const String _count = 'count';
+  static const String _countUnit = 'countUnit';
+  static const String _feeling = 'feeling';
+
+  Future<int> save(Goal goal) async {
+    final Database db = await getDatabase();
+    Map<String, dynamic> goalMap = _toMap(goal);
+    return db.insert(_tableName, goalMap);
+  }
+
+  Map<String, dynamic> _toMap(Goal goal) {
+    final Map<String, dynamic> goalMap = Map();
+    goalMap['$_title'] = goal.title;
+    goalMap['$_count'] = goal.count;
+    goalMap['$_countUnit'] = goal.countUnit;
+    goalMap['$_feeling'] = goal.feeling;
+    return goalMap;
+  }
+
+  Future<List<Goal>> findAll() async {
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> queryResult = await db.query(_tableName);
+    List<Goal> goals = _toList(queryResult);
+    return goals;
+  }
+
+  List<Goal> _toList(List<Map<String, dynamic>> queryResult) {
+    final List<Goal> goals = List();
+    for (Map<String, dynamic> row in queryResult) {
+      final Goal goal = Goal(
+        row['$_id'],
+        row['$_title'],
+        row['$_count'],
+        row['$_countUnit'],
+        row['$_feeling'],
+      );
+      goals.add(goal);
+    }
+    return goals;
+  }
+}
--- test/models/goal_test.dart
+++ test/models/goal_test.dart
@@ -1,38 +1,39 @@
-//import 'package:build/models/goal.dart';
-//import 'package:flutter_test/flutter_test.dart';
-//
-//main() {
-//    group('Goal model tests', () {
-//      test('Goal count initial value should be zero', () {
-//        final goal = Goal('dumb title');
-//
-//        expect(goal.count, 0);
-//      });
-//
-//      test('Increment should add count', () {
-//        final goal = Goal('dumb title');
-//
-//        goal.increment();
-//
-//        expect(goal.count, 1);
-//      });
-//
-//      test('Decrement when count above zero should decrease count', () {
-//        final goal = Goal('dumb title');
-//
-//        goal.count = 5;
-//
-//        goal.decrement();
-//
-//        expect(goal.count, 4);
-//      });
-//
-//      test('Decrement when count equals to zero should not decrease count', () {
-//        final goal = Goal('dumb title');
-//
-//        goal.decrement();
-//
-//        expect(goal.count, 0);
-//      });
-//    });
-//}
+import 'package:build/models/goal.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+    group('Goal model tests', () {
+
+      Goal goal;
+
+      setUp(() {
+        goal = Goal(0, 'title', 0, 'countUnit', 1);
+      });
+
+      test('Goal count initial value should be zero', () {
+
+
+        expect(goal.count, 0);
+      });
+
+      test('Increment should add count', () {
+        goal.increment();
+
+        expect(goal.count, 1);
+      });
+
+      test('Decrement when count above zero should decrease count', () {
+        goal.count = 5;
+
+        goal.decrement();
+
+        expect(goal.count, 4);
+      });
+
+      test('Decrement when count equals to zero should not decrease count', () {
+        goal.decrement();
+
+        expect(goal.count, 0);
+      });
+    });
+}
