615facdb516a857fe71fc01305cc0b712b2ca3f8
Breno Baronte <brenobaronte@hotmail.com>
2020-06-14 23:08:23 -0300

    Added new goal form with validation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -52,13 +52,169 @@ class Home extends StatelessWidget {
               return Text('Unknown error');
             }),
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        child: Icon(
+          Icons.add,
+          color: Colors.black,
+          size: 30,
+        ),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return Dialog(
+                backgroundColor: Colors.grey[900],
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                child: AddGoalDialog(),
+              );
+            },
+          );
+        },
+      ),
     );
   }
+}
 
-//  Widget _createItem(Goal goal) {
-//    return Padding(
-//      padding: const EdgeInsets.all(16.0),
-//      child: GoalTile(goal),
-//    );
-//  }
+class AddGoalDialog extends StatelessWidget {
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              elevation: 0,
+              title: Text('New Goal'),
+              centerTitle: true,
+            ),
+            // title input area
+            Flexible(
+              child: Container(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Wrap(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Title of the new goal'
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter a title';
+                            }
+                            return null;
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // count input area
+            Flexible(
+              child: Container(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Wrap(
+                      children: <Widget>[
+                        TextFormField(
+                          initialValue: '0',
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                              hintText: 'Initial count value'
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter a count value';
+                            }
+                            try {
+                              var number = int.parse(value);
+                            } catch (e) {
+                              return 'Please enter a numeric value';
+                            }
+                            return null;
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // count unit area
+            Flexible(
+              child: Container(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Wrap(
+                      children: <Widget>[
+                        TextFormField(
+                          initialValue: 'Days',
+                          decoration: InputDecoration(
+                              hintText: 'Count measure'
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter a count measure';
+                            }
+                            return null;
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // feeling area // todo: do it right
+            Flexible(
+              child: Container(
+                child: Center(
+                  child: Column(
+                    children: <Widget>[
+                      Text('Feeling', style: TextStyle(fontSize: 20.0)),
+                      SizedBox(height: 10.0),
+                      Icon(Icons.sentiment_very_satisfied),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // create button
+            Flexible(
+              child: Center(
+                child: MaterialButton(
+                    color: Colors.amber,
+                    elevation: 1,
+                    child: Text(
+                      'Create',
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        print('object');
+                      }
+                      else {
+                        print ('xi');
+                      }
+                    }),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
